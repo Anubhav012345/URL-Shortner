d6334b6f1db1236c81e3a0eb9ed36337f7231687
Anubhav Srivastava <2k22cscys2211541@gmail.com>
2026-06-03 22:51:15 +0530

    fixed api url

--- FRONTEND/src/utils/axiosInstance.js
+++ FRONTEND/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://url-shortner-backend-s7q5.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
